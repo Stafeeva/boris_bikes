135d0da346297bb35b9e81be29c92d04ca96908d
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-02-16 21:06:46 +0000

    Challenge day 3

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,17 @@
+require_relative 'bike'
+
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
+   fail 'There are no more bikes!' unless @bike
+   @bike
   end
+
+  def dock(bike)
+    fail "There is already docked bike!" if @bike
+    @bike = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,50 @@
 require 'docking_station'
 
+
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike). to eq bike
+      end
+    end
+
+  it "returns a new instance of Bike.new" do
+    #bike = subject.release_bike
+    bike = Bike.new
+    expect(bike).to be_working
+  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'return docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it "docking station raises error when there are no bikes to release" do
+      expect { subject.release_bike }.to raise_error("There are no more bikes!")
+    end
+  end
+
+  describe '#dock' do
+    it "raises an error when there is already docked bike" do
+      bike_one = Bike.new
+      subject.dock(bike_one)
+      bike_two = Bike.new
+      expect { subject.dock(bike_two) }.to raise_error("There is already docked bike!")
+    end
+  end
+
 end
