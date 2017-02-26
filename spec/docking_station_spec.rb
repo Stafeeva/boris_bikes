92ca5b81dcba1c95a388188d2b9c628c22c13ae2
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-02-26 09:35:10 +0000

    Commits

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,29 +2,9 @@ require 'docking_station'
 
 describe DockingStation do
 
-  describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-
-      end
-    end
-
   it { is_expected.to respond_to(:dock).with(1).argument }
 
   it { is_expected.to respond_to(:bikes) }
-
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)[0]).to eq bike
-  end
-
-  it 'return docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes[0]).to eq bike
-  end
 
   describe '#release_bike' do
     it "docking station raises error when there are no bikes to release" do
@@ -34,16 +14,16 @@ describe DockingStation do
 
   describe '#dock' do
     it "raises an error when there is already docked bike" do
-      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
-      expect { subject.dock(Bike.new) }.to raise_error("The docking station is full!")
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(double(:bike))}
+      expect { subject.dock(double(:bike)) }.to raise_error("The docking station is full!")
     end
   end
 
   describe '#dock' do
     it 'should ....' do
       station = DockingStation.new(10)
-      10.times { station.dock(Bike.new) }
-      expect { station.dock(Bike.new) }.to raise_error("The docking station is full!")
+      10.times { station.dock(double(:bike)) }
+      expect { station.dock(double(:bike)) }.to raise_error("The docking station is full!")
     end
   end
 
